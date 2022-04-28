f0689e137047491d1b38fa4096e852f491c7d497
Luc Smith <93154131+LucS12@users.noreply.github.com>
2022-04-28 15:21:25 -0400

    Update get_prices.py

--- data/price-data/get_prices.py
+++ data/price-data/get_prices.py
@@ -14,7 +14,12 @@ end='20220428'
 #Get stock symbols:
 sym_data = pd.read_csv('esg_scores.csv', index_col='ticker')
 symbols = list(sym_data.index)
+spx = 'SPX Index'
 
 #Get historical data:
-prices = con.bdh(symbols, p, start, end,
-                 elms=[('periodicitySelection', 'MONTHLY')])
+prices = con.bdh(symbols, p, start, end)
+spx_prices = con.bdh(spx, p, start, end)
+
+#Place price data into CSV files:
+prices.to_csv('daily_prices.csv')
+spx_prices.to_csv('daily_spx.csv')
